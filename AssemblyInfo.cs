30ccafc40693df4cb777aa7b445a1e5d49b6a7e3
Zero <31504103+Ballcracker@users.noreply.github.com>
2017-08-31 18:48:02 +1000

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: Extension]
+[assembly: AssemblyTitle("Safe Exam Browser")]
+[assembly: AssemblyDescription("Safe Exam Browser")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ETH Zurich")]
+[assembly: AssemblyProduct("Safe Exam Browser")]
+[assembly: AssemblyCopyright("Copyright ©  2010-2016 ETH Zurich, Educational Development and Technology (LET)")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f378d07d-ef62-43c8-994b-e2a5788f7af2")]
+[assembly: AssemblyInformationalVersion("2.1.5")]
+[assembly: AssemblyVersion("2.1.1.76")]
